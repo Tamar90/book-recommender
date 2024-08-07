28ddc4b59df0b8513cefa16d9b430918cb39478e
Tamar90 <tdg400090@gmail.com>
2024-08-07 21:55:24 +0300

    Add catch error

--- src/app/states/book-resolver.ts
+++ src/app/states/book-resolver.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { map, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { BookService } from '../services/book.service';
 import { Book } from '../models/book.model';
 import { BookStateService } from './book-state-service';
+import { Router, RouterModule } from '@angular/router';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ import { BookStateService } from './book-state-service';
 })
 export class BookResolver implements Resolve<Book[]> {
 
-    constructor(private bookService: BookService, private bookStateService: BookStateService) { }
+    constructor(private bookService: BookService, private bookStateService: BookStateService, private router: Router) { }
 
     resolve(): Observable<Book[]> {
         const cachedBooks = this.bookStateService.getBooks();
@@ -20,7 +21,12 @@ export class BookResolver implements Resolve<Book[]> {
         } else {
             return this.bookService.searchBooks('bestsellers&limit=60').pipe(
                 map(data => data.docs.map((item: any) => new Book(item))),
-                tap(books => this.bookStateService.setBooks(books))
+                tap(books => this.bookStateService.setBooks(books)),
+                catchError(error => {
+                    console.error('Error fetching books:', error);
+                    this.router.navigate(['/book-not-found']);
+                    return of([]);
+                })
             );
         }
     }
